bfdc54229c771926c8da1edb28fb3925c11c3fdc
chandradev <Chandradev_Prasad@Dellteam.com>
2020-06-07 16:16:37 +0530

    Add project files.

---
+++ BulkyBook.Models/SubSubCategory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class SubSubCategory
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+        [Display(Name = "SubCategory")]
+        public int SubCategoryId { get; set; }
+
+        [ForeignKey("SubCategoryId")]
+        public virtual SubCategory SubCategory { get; set; }
+    }
+}
---
+++ BulkyBook/wwwroot/js/CascadingDropdown.js
@@ -0,0 +1,30 @@
+﻿
+    $(function () {
+        $("#Product_CategoryId").change(function () {
+
+            debugger;
+            $.getJSON("/Admin/Product/GetSubCategory", { catId: $("#Product_CategoryId").val() }, function (d) {
+                alert("Test");
+                var row = "";
+                //$("#Product_SubCategoryId").empty();
+                $.each(d, function (i, v) {
+                    row += "<option value=" + v.value + ">" + v.text + "</option>";
+                    alert(v.value);
+                });
+
+                $("#Product_SubCategoryId").html(row);
+
+            })
+        })
+            $("#Product_SubCategoryId").change(function () {
+                debugger;
+                $.getJSON("/Admin/Product/GetMiniCategory", {subCateId: $("#Product_SubCategoryId").val() }, function (d) {
+                    var row = "";
+                    //$("#Product.SubSubId").empty();
+                    $.each(d, function (i, v) {
+        row += "<option value=" + v.value + ">" + v.text + "</option>";
+                    });
+                    $("#Product.SubSubId").html(row);
+                })
+            })
+        })
